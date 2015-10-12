10511ed6b8dc8f94d2c0176a9fc429a4b430cccf
Odonno <bottiau.david@laposte.net>
2015-10-12 20:51:13 +0200

    feat(app): create iOS project

---
+++ GitterSharp/GitterSharp.iOS/Helpers/HttpHelper.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GitterSharp.Helpers
+{
+    internal static class HttpHelper
+    {
+        public static async Task<T> GetAsync<T>(this HttpClient httpClient, string url)
+        {
+            using (httpClient)
+            {
+                var response = await httpClient.GetAsync(new Uri(url));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+
+                throw new Exception();
+            }
+        }
+
+        public static async Task PostAsync(this HttpClient httpClient, string url, HttpContent content)
+        {
+            using (httpClient)
+            {
+                var response = await httpClient.PostAsync(new Uri(url), content);
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception();
+            }
+        }
+        public static async Task<T> PostAsync<T>(this HttpClient httpClient, string url, HttpContent content)
+        {
+            using (httpClient)
+            {
+                var response = await httpClient.PostAsync(new Uri(url), content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+
+                throw new Exception();
+            }
+        }
+
+        public static async Task PutAsync(this HttpClient httpClient, string url, HttpContent content)
+        {
+            using (httpClient)
+            {
+                var response = await httpClient.PutAsync(new Uri(url), content);
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception();
+            }
+        }
+        public static async Task<T> PutAsync<T>(this HttpClient httpClient, string url, HttpContent content)
+        {
+            using (httpClient)
+            {
+                var response = await httpClient.PutAsync(new Uri(url), content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+
+                throw new Exception();
+            }
+        }
+    }
+}
