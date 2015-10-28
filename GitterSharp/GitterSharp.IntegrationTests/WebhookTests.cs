6ae85c3f492162d5c9229959d7c0e09c783a8374
Odonno <bottiau.david@laposte.net>
2015-10-28 21:34:21 +0100

    tests(webhook): complete PostAsync webhook method

--- GitterSharp/GitterSharp.IntegrationTests/WebhookTests.cs
+++ GitterSharp/GitterSharp.IntegrationTests/WebhookTests.cs
@@ -1,4 +1,5 @@
 ﻿using GitterSharp.Services;
+using GitterSharp.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
@@ -14,7 +15,20 @@ namespace GitterSharp.IntegrationTests
             IWebhookService webhookService = new WebhookService();
 
             // Act
-            bool result = await webhookService.PostAsync("https://webhooks.gitter.im/e/cdf519d88a935d54a6d2", "A simple test");
+            bool result = await webhookService.PostAsync("https://webhooks.gitter.im/e/cdf519d88a935d54a6d2", "A simple message");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task Can_Post_Simple_Error_Message()
+        {
+            // Arrange
+            IWebhookService webhookService = new WebhookService();
+
+            // Act
+            bool result = await webhookService.PostAsync("https://webhooks.gitter.im/e/cdf519d88a935d54a6d2", "A simple error message", MessageLevel.Error);
 
             // Assert
             Assert.IsTrue(result);
