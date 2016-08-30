13fba29ffa3f50467d13fad0ab8b419e0294939c
Odonno <bottiau.david@laposte.net>
2016-08-30 20:33:15 +0200

    refactor(webhook): refactor reactive webhook implementation

---
+++ GitterSharp/GitterSharp.IntegrationTests/ReactiveWebhookTests.cs
@@ -0,0 +1,59 @@
+﻿using GitterSharp.Model;
+using GitterSharp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitterSharp.IntegrationTests
+{
+    [TestClass]
+    public class ReactiveWebhookTests
+    {
+        [TestMethod]
+        public async Task Can_Post_Simple_Message()
+        {
+            // Arrange
+            IReactiveWebhookService reactiveWebhookService = new ReactiveWebhookService();
+
+            // Act
+            reactiveWebhookService.Post("https://webhooks.gitter.im/e/cdf519d88a935d54a6d2", "A simple message")
+                .Subscribe((result) =>
+                {
+                    // Assert
+                    Assert.IsTrue(result);
+                },
+                (error) =>
+                {
+                    // Assert
+                    Assert.Fail();
+                });
+
+            await Task.Delay(5000);
+        }
+
+        [TestMethod]
+        public async Task Can_Post_Simple_Error_Message()
+        {
+            // Arrange
+            IReactiveWebhookService reactiveWebhookService = new ReactiveWebhookService();
+
+            // Act
+            reactiveWebhookService.Post("https://webhooks.gitter.im/e/cdf519d88a935d54a6d2", "A simple error message", MessageLevel.Error)
+                .Subscribe((result) =>
+                {
+                    // Assert
+                    Assert.IsTrue(result);
+                },
+                (error) =>
+                {
+                    // Assert
+                    Assert.Fail();
+                });
+
+            await Task.Delay(5000);
+        }
+    }
+}
--- GitterSharp/GitterSharp.NetFramework/Services/ReactiveWebhookService.cs
+++ GitterSharp/GitterSharp.NetFramework/Services/ReactiveWebhookService.cs
@@ -3,14 +3,6 @@ using GitterSharp.Model;
 using System.Collections.Generic;
 using System.Reactive.Threading.Tasks;
 using System.Reactive.Linq;
-#if __IOS__ || __ANDROID__ || NET45
-using System.Net.Http;
-using System.Net.Http.Headers;
-#endif
-#if NETFX_CORE
-using Windows.Web.Http;
-using Windows.Web.Http.Headers;
-#endif
 
 namespace GitterSharp.Services
 {
@@ -18,24 +10,7 @@ namespace GitterSharp.Services
     {
         #region Fields
 
-        private HttpClient HttpClient
-        {
-            get
-            {
-                var httpClient = new HttpClient();
-
-#if __IOS__ || __ANDROID__ || NET45
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
-                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
-#endif
-#if NETFX_CORE
-                httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
-                httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
-#endif
-
-                return httpClient;
-            }
-        }
+        private IWebhookService _webhookService = new WebhookService();
 
         #endregion
 
@@ -44,36 +19,7 @@ namespace GitterSharp.Services
 
         public IObservable<bool> Post(string url, string message, MessageLevel level = MessageLevel.Info)
         {
-            // Create an HttpClient and send content payload
-            using (var httpClient = HttpClient)
-            {
-#if __IOS__ || __ANDROID__ || NET45
-                var content = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    {"message", message},
-                    {"level", level.ToString().ToLower()}
-                });
-#endif
-#if NETFX_CORE
-                var content = new HttpFormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    {"message", message},
-                    {"level", level.ToString().ToLower()}
-                });
-#endif
-
-#if __IOS__ || __ANDROID__ || NET45
-                return httpClient.PostAsync(new Uri(url), content)
-                    .ToObservable()
-                    .Select(response => response.IsSuccessStatusCode);
-#endif
-#if NETFX_CORE
-                return httpClient.PostAsync(new Uri(url), content)
-                    .AsTask()
-                    .ToObservable()
-                    .Select(response => response.IsSuccessStatusCode);
-#endif
-            }
+            return _webhookService.PostAsync(url, message, level).ToObservable();
         }
 
         #endregion
